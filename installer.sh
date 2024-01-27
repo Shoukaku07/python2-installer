d2c671d736485de7004a178d36de4d813584870b
shoukaku07 <132239141+Shoukaku07@users.noreply.github.com>
2024-01-27 15:07:08 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,6 +4,5 @@ tar xzf Python-2.7.9.tgz
 cd Python-2.7.9
 ./configure --enable-optimizations
 make altinstall
-cd .. && rm -rf Python-2.7.9
 sudo ln -sfn '/usr/local/bin/python2.7' '/usr/bin/python2'
 echo "python2 has been installed!"
